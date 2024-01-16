f135dd2b7cd41d693eb93d118b7fc086aac3cfcf
Md Raziur Rahaman Ronju <raziur.rahaman.ronju@gmail.com>
2024-01-17 00:30:39 +0600

    project setup

---
+++ pages/_app.jsx
@@ -0,0 +1,20 @@
+import Theme from "@/context/FontContext";
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Theme>
+        <Toaster
+          toastOptions={{
+            style: {
+              fontFamily: "arial",
+            },
+          }}
+        />
+      <Component {...pageProps} />
+    </Theme>
+  );
+}
+
+export default MyApp;
